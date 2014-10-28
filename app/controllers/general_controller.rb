8d2652bcd6c6bd6b670e7c39a4f73069d055e1f9
Taufiq Muhammadi <waruboy@yahoo.com>
2014-10-29 06:02:17 +0700

    passing subscription to payment

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -3,6 +3,12 @@ class GeneralController < ApplicationController
   end
 
   def payment
+    @preferences = params[:preferences]
+    @lunches = params[:lunch]
+    @dinners = params[:dinner]
+    @email = params[:email]
+    @zipcode = params[:zipcode]
+    @phone = params[:phone]
   end
 
   def subscribe 
